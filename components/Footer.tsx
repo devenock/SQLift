11092c3bede49f19d288cea3fd7221cdf14433e8
Trend20 <enockomondi305@gmail.com>
2024-12-22 09:05:30 +0300

    footer refactor

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="bg-gray-900 py-8">
       <div className="container mx-auto px-4 text-center text-gray-400">
-        <p>&copy; 2023 SQLMaster. All rights reserved.</p>
+        <p>&copy; {new Date().getFullYear()} SQLMaster. All rights reserved.</p>
       </div>
     </footer>
   );
